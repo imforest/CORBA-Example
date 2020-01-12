e85fa4ddb5548f7dd3da7c0c1c9e39b12bfdf868
kkb <imforest@gmail.com>
2020-01-12 13:04:03 +0900

    Update echoString service
    - 동시 접근 client를 1개로 제한

--- src/EchoServer.java
+++ src/EchoServer.java
@@ -1,8 +1,44 @@
 import EchoApp.EchoPOA;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class EchoServer extends EchoPOA {
+
+    final AtomicInteger runningCount = new AtomicInteger(0);
+    final Integer limit = 1;
+    final String ErrorMessage = "limit exceeded";
+
     @Override
     public String echoString() {
-        return "Hello World!!!!!!!";
+        if (runningCount.incrementAndGet() <= limit) { //increment number of clients and check
+
+            doSomeThing();
+            runningCount.decrementAndGet();
+            return "Hello World!!!!!!!";
+
+        } else {
+            runningCount.decrementAndGet();
+            System.out.println(ErrorMessage);
+            return ErrorMessage;
+        }
+
+    }
+
+    private void doSomeThing() {
+        try {
+            for (int i = 1; i < 5; i++) {
+                Thread.sleep(i * 1000);
+                System.out.println("sleep " + i);
+            }
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
